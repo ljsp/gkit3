f1f3f6ea8fc94160eab2bff2f57642219c850aa1
Lucas Petit <lucas.petit@etu.univ-lyon1.fr>
2023-05-30 11:20:42 +0200

    Chargement de fichier obj

--- projets/tp2.cpp
+++ projets/tp2.cpp
@@ -1,9 +1,9 @@
-// exemple de code test a compiler pour verifier que tout est ok
 #include "color.h"
 #include "image.h"
 #include "image_io.h"
 #include "mesh_io.h"
 #include "vec.h"
+#include "mat.h"
 #include <vec.h>
 #include <array>
 
@@ -13,7 +13,7 @@ void movePositions(std::vector<Point>& positions, Vector& direction) {
 	}
 }
 
-void boundingBox(const std::vector<Point>& positions, Point& pmin, Point& pmax) {
+void getBoundingBox(const std::vector<Point>& positions, Point& pmin, Point& pmax) {
 	pmin = positions[0];
 	pmax = positions[0];
     for (unsigned i = 1; i < positions.size(); i++) {
@@ -22,18 +22,45 @@ void boundingBox(const std::vector<Point>& positions, Point& pmin, Point& pmax) 
 	}
 }
 
-void drawTriangles(Image& image, const std::vector<Point>& positions) {
+bool isFaceCulled(std::array<Point, 3> currentTriangle, Point origin) {
+	
+    Vector edge1 = currentTriangle[1] - currentTriangle[0];
+	Vector edge2 = currentTriangle[2] - currentTriangle[0];
+	Vector triangleNormal = cross(edge1, edge2);
+
+	if (dot(triangleNormal, origin - currentTriangle[0]) < 0) {
+		return true;
+	}
+	return false;
+}
+
+bool isRayInTriangle(std::array<Vector, 3>& currentTriangleNormals, Vector& rayDirection) {
+    
+    std::array<float, 3> dotProducts;
+    
+    for (int i = 0; i < 3; i++) {
+        dotProducts[i] = dot(currentTriangleNormals[i], rayDirection);
+    }
+    return (dotProducts[0] >= 0) && (dotProducts[1] >= 0) && (dotProducts[2] >= 0);
+}
+
+void drawTriangles(Image& image, const MeshIOData& data) {
+    
     std::array<Point, 3> currentTriangle;
     std::array<Vector, 3> currentTriangleNormals;
-    std::array<float, 3> V, lambdas;
+    std::array<float, 3> dotProducts, lambdas;
     Point origin(0, 0, 0);
-
+    
     // parcours tous les triangles
-    for (unsigned i = 0; i + 2 < positions.size(); i += 3)
+    for (unsigned i = 0; i + 2 < data.indices.size(); i += 3)
     {
-        currentTriangle[0] = positions[i + 0];
-        currentTriangle[1] = positions[i + 1];
-        currentTriangle[2] = positions[i + 2];
+        currentTriangle[0] = data.positions[data.indices[i + 0]];
+        currentTriangle[1] = data.positions[data.indices[i + 1]];
+        currentTriangle[2] = data.positions[data.indices[i + 2]];
+
+		if (isFaceCulled(currentTriangle, origin)) {
+            continue;
+		}
 
         for (int i = 0; i < 3; i++) {
             currentTriangleNormals[i] = normalize(cross(currentTriangle[(i + 2) % 3] - origin, currentTriangle[(i + 1) % 3] - currentTriangle[(i + 2) % 3]));
@@ -41,22 +68,15 @@ void drawTriangles(Image& image, const std::vector<Point>& positions) {
 
         for (int py = 0; py < image.height(); py++) {
             for (int px = 0; px < image.width(); px++) {
-                // rayon
                 float x = (static_cast<float>(px) / image.width()) * 2 - 1;
                 float y = (static_cast<float>(py) / image.height()) * 2 - 1;
                 float z = -1;
-                Point p = Point(x, y, z);
-                Vector d = Vector(origin, p);
+                Point rayOrigin = Point(x, y, z);
+                Vector rayDirection = normalize(Vector(origin, rayOrigin));
 
-                for (int i = 0; i < 3; i++) {
-                    V[i] = dot(currentTriangleNormals[i], d);
-                }
-
-                if ((V[0] >= 0) && (V[1] >= 0) && (V[2] >= 0)) {
-                    for (int i = 0; i < 3; i++) {
-                        lambdas[i] = V[i] / (V[0] + V[1] + V[2]);
-                    }
-                    image(px, py) = lambdas[0] * Red() + lambdas[1] * Green() + lambdas[2] * Blue();
+				if (isRayInTriangle(currentTriangleNormals, rayDirection)) {
+                    Color color = data.materials.materials[data.material_indices[i / 3]].diffuse;
+                    image(px, py) = color;
                 }
             }
         }
@@ -64,23 +84,20 @@ void drawTriangles(Image& image, const std::vector<Point>& positions) {
     }
 }
 
-Image read_mesh(const char* filename) {
+Image load_mesh(const char* filename) {
    
     Image image(1024, 1024);
-    std::vector<Point> positions;
     Point pmin, pmax;
-
-    assert(read_positions(filename, positions) != -1);
+	MeshIOData data = read_meshio_data(filename);
     
-    movePositions(positions, Vector(0, -2, -4));
+    movePositions(data.positions, Vector(-300, -300, -800));
+    //movePositions(data.positions, Vector(0, -2, -8));
+    getBoundingBox(data.positions, pmin, pmax);
+    
+    drawTriangles(image, data);
 
-    boundingBox(positions, pmin, pmax);
-
-    printf("%d triangles\n", int(positions.size() / 3));
+    printf("%d triangles\n", int(data.positions.size() / 3));
     printf("bounds [%f %f %f]x[%f %f %f]\n", pmin.x, pmin.y, pmin.z, pmax.x, pmax.y, pmax.z);
-
-    drawTriangles(image, positions);
-
     std::cout << "fin de processus" << std::endl;
 
     return image;
@@ -88,8 +105,7 @@ Image read_mesh(const char* filename) {
 
 int main() {
     
-    const char* filename = "data/robot.obj";
-    Image image = read_mesh(filename);
-    write_image(image, "image_Robot.png"); // par defaut en .png
+    Image image = load_mesh("data/geometry.obj");
+    write_image(image, "image_Scene.png"); // par defaut en .png
     return 0;
 }
